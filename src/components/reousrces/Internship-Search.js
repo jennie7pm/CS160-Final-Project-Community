63920e50509e7693052155bb60f08ba2af89c443
sekar <sekarnareswari@berkeley.edu>
2023-08-05 04:53:59 -0700

    added open position component

--- src/components/reousrces/Internship-Search.js
+++ src/components/reousrces/Internship-Search.js
@@ -8,8 +8,31 @@ import HandshakeLogo from "./images/logos/handshake.png";
 import OttaLogo from "./images/logos/otta.png";
 import ZiprecruiterLogo from "./images/logos/ziprecruiter.png";
 import GoldmanSachsLogo from "./images/logos/goldmansachs.png";
-
 import { useCallback } from "react";
+
+function OpenPosition({ company, location, position }) {
+  return (
+    <div className="position-card">
+      <div className="company-container">
+        <img className="icon" alt={company.name} src={company.logo}></img>
+        <p>{company.name}</p>
+      </div>
+      <div className="location-container">
+        {location.map((loc, index) => (
+          <p key={index}>{loc}</p>
+        ))}
+      </div>
+      <div className="link-container">
+        {position.map((pos, index) => (
+          <button className="link-button" key={index}>
+            <p>{pos}</p>
+            <img className="small-icon" src={LinkIcon}></img>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export function InternshipSearch() {
   const openLinkedIn = useCallback(() => {
@@ -100,54 +123,94 @@ export function InternshipSearch() {
             <h2>Open Positions</h2>
           </div>
           <div className="resources-container">
-            <div className="position-card">
-              <img
-                className="icon"
-                alt="Goldman Sachs logo"
-                src={GoldmanSachsLogo}
-              ></img>
-              <p>Goldman Sachs</p>
-              <div className="column-container">
-                <p>Multiple locations</p>
-              </div>
-              <div className="column-container">
-                <button className="link-button">
-                  <p>Analyst</p>
-                  <img className="small-icon" src={LinkIcon}></img>
-                </button>
-              </div>
-            </div>
-            <div className="position-card">
-              <img
-                className="icon"
-                alt="Goldman Sachs logo"
-                src={GoldmanSachsLogo}
-              ></img>
-              <div className="column-container">
-                <p>Walmart</p>
-              </div>
-              <div className="column-container">
-                <p>Multiple locations</p>
-              </div>
-              <div className="column-container">
-                <button className="link-button">
-                  <p>Analyst</p>
-                  <img className="small-icon" src={LinkIcon}></img>
-                </button>
-                <button className="link-button">
-                  <p>Analyst</p>
-                  <img className="small-icon" src={LinkIcon}></img>
-                </button>
-                <button className="link-button">
-                  <p>Analyst</p>
-                  <img className="small-icon" src={LinkIcon}></img>
-                </button>
-                <button className="link-button">
-                  <p>Analyst</p>
-                  <img className="small-icon" src={LinkIcon}></img>
-                </button>
-              </div>
-            </div>
+            <OpenPosition
+              company={{ name: "Goldman Sachs", logo: GoldmanSachsLogo }}
+              location={["Multiple locations"]}
+              position={["Analyst"]}
+            />
+            <OpenPosition
+              company={{ name: "KPMG", logo: GoldmanSachsLogo }}
+              location={["Multiple locations"]}
+              position={["Engineering & IT"]}
+            />
+            <OpenPosition
+              company={{ name: "Optiver", logo: GoldmanSachsLogo }}
+              location={["Chicago, IL", "Austin, TX"]}
+              position={[
+                "Quantitative Trading",
+                "HFT Research",
+                "Software Engineering",
+              ]}
+            />
+            <OpenPosition
+              company={{ name: "Walmart", logo: GoldmanSachsLogo }}
+              location={["Multiple locations"]}
+              position={[
+                "Software Engineering",
+                "Systems Engineering",
+                "Cybersecurity",
+                "Data Science",
+              ]}
+            />
+            <OpenPosition
+              company={{ name: "Apple", logo: GoldmanSachsLogo }}
+              location={["Multiple locations"]}
+              position={["Software Engineering", "ML/AI", "Engineering PM"]}
+            />
+            <OpenPosition
+              company={{ name: "Neuralink", logo: GoldmanSachsLogo }}
+              location={["Fremont, CA", "Austin, TX"]}
+              position={["Software Engineering"]}
+            />
+            <OpenPosition
+              company={{ name: "Certik", logo: GoldmanSachsLogo }}
+              location={["Multiple locations"]}
+              position={["Development", "Platform Engineering", "Full-stack"]}
+            />
+            <OpenPosition
+              company={{ name: "D.E. Shaw", logo: GoldmanSachsLogo }}
+              location={["New York, NY"]}
+              position={[
+                "Software Development",
+                "Systems Technical PM",
+                "Systems Administration",
+                "Systems Technology",
+              ]}
+            />
+            <OpenPosition
+              company={{ name: "Bank of America", logo: GoldmanSachsLogo }}
+              location={["Multiple locations"]}
+              position={["Global Technology Analyst"]}
+            />
+            <OpenPosition
+              company={{ name: "Protiviti", logo: GoldmanSachsLogo }}
+              location={["Phoenix, AZ"]}
+              position={["Technology Consulting"]}
+            />
+            <OpenPosition
+              company={{ name: "Mercedes-Benz", logo: GoldmanSachsLogo }}
+              location={["Sunnyvale, CA"]}
+              position={["Data Products"]}
+            />
+            <OpenPosition
+              company={{ name: "LUMEN", logo: GoldmanSachsLogo }}
+              location={["Sunnyvale, CA"]}
+              position={["Various positions"]}
+            />
+            <OpenPosition
+              company={{ name: "JPMorgan Chase", logo: GoldmanSachsLogo }}
+              location={["Multiple locations"]}
+              position={["Software Engineering", "AI and Data Science"]}
+            />
+            <OpenPosition
+              company={{ name: "Citadel", logo: GoldmanSachsLogo }}
+              location={["Multiple locations"]}
+              position={[
+                "Software Engineering",
+                "Investment and Trading",
+                "Quant Research",
+              ]}
+            />
           </div>
         </div>
       </div>
