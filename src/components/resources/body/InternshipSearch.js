4074f7d857dcb3690b0e900950c0a88ea249ab7c
sekar <sekarnareswari@berkeley.edu>
2023-08-06 23:16:02 -0700

    finished internship search

--- src/components/resources/body/InternshipSearch.js
+++ src/components/resources/body/InternshipSearch.js
@@ -1,42 +1,73 @@
 import "./InternshipSearch.css";
+import data from "./openinternships.json";
+
 import Tool from "../Tool";
+
 import LinkIcon from "../images/icons/link arrow.png";
 import InternshipSearchIcon from "../images/icons/internshipsearch.png";
+
 import LinkedInLogo from "../images/logos/linkedin.png";
 import GlassdoorLogo from "../images/logos/glassdoor.png";
 import IndeedLogo from "../images/logos/indeed.png";
 import HandshakeLogo from "../images/logos/handshake.png";
 import OttaLogo from "../images/logos/otta.png";
 import ZiprecruiterLogo from "../images/logos/ziprecruiter.png";
-import GoldmanSachsLogo from "../images/logos/goldmansachs.png";
 
-function OpenPosition({ company, location, position, links }) {
+function OpenPosition() {
   return (
-    <div className="position-card">
-      <div className="company-container">
-        <img className="icon" alt={company.name} src={company.logo}></img>
-        <p>{company.name}</p>
-      </div>
-      <div className="location-container">
-        {location.map((loc, index) => (
-          <p key={index}>{loc}</p>
-        ))}
-      </div>
-      <div className="link-container">
-        {position.map((pos, index) => (
-          <button
-            className="link-button"
-            key={index}
-            onClick={() => window.open(links[index])}
-          >
-            <p>{pos}</p>
-            <img className="small-icon" src={LinkIcon}></img>
-          </button>
-        ))}
-      </div>
+    <div className="resources-container">
+      {data.map((companyData, index) => (
+        <div className="position-card" key={index}>
+          <div className="company-container">
+            <p>{companyData.company}</p>
+          </div>
+          <div className="location-container">
+            {companyData.locations.map((loc, index) => (
+              <p key={index}>{loc}</p>
+            ))}
+          </div>
+          <div className="link-container">
+            {companyData.roles.map((role, index) => (
+              <button
+                className="link-button"
+                key={index}
+                onClick={() => window.open(role.link)}
+              >
+                <p>{role.role}</p>
+                <img
+                  className="small-icon"
+                  src={LinkIcon}
+                  alt="Link Icon"
+                ></img>
+              </button>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
+
+const tools = [
+  { name: "LinkedIn", logo: LinkedInLogo, link: "https://linkedin.com/" },
+  {
+    name: "Glassdoor",
+    logo: GlassdoorLogo,
+    link: "https://www.glassdoor.com/",
+  },
+  { name: "Indeed", logo: IndeedLogo, link: "https://www.indeed.com/" },
+  {
+    name: "Handshake",
+    logo: HandshakeLogo,
+    link: "https://joinhandshake.com/",
+  },
+  { name: "Otta", logo: OttaLogo, link: "https://otta.com/" },
+  {
+    name: "Ziprecruiter",
+    logo: ZiprecruiterLogo,
+    link: "https://www.ziprecruiter.com/",
+  },
+];
 
 export function InternshipSearch() {
   return (
@@ -54,130 +85,22 @@ export function InternshipSearch() {
           <h2>Tools</h2>
         </div>
         <div className="tool-container">
-          <Tool
-            name={"LinkedIn"}
-            logo={LinkedInLogo}
-            link={"https://linkedin.com/"}
-          />
-          <Tool
-            name={"Glassdoor"}
-            logo={GlassdoorLogo}
-            link={"https://www.glassdoor.com/"}
-          />
-          <Tool
-            name={"Indeed"}
-            logo={IndeedLogo}
-            link={"https://www.indeed.com/"}
-          />
-          <Tool
-            name={"Handshake"}
-            logo={HandshakeLogo}
-            link={"https://joinhandshake.com/"}
-          />
-          <Tool name={"Otta"} logo={OttaLogo} link={"https://otta.com/"} />
-          <Tool
-            name={"Ziprecruiter"}
-            logo={ZiprecruiterLogo}
-            link={"https://www.ziprecruiter.com/"}
-          />
+          {tools.map((tool) => (
+            <Tool
+              name={tool.name}
+              logo={tool.logo}
+              link={tool.link}
+              key={tool.name}
+            />
+          ))}
         </div>
       </div>
       <div className="section">
         <div className="section-header">
-          <h2>Open Positions</h2>
+          <h2>Summer 2024 Open Positions</h2>
         </div>
-        <div className="resources-container">
-          <OpenPosition
-            company={{ name: "Goldman Sachs", logo: GoldmanSachsLogo }}
-            location={["Multiple locations"]}
-            position={["Analyst"]}
-          />
-          <OpenPosition
-            company={{ name: "KPMG", logo: GoldmanSachsLogo }}
-            location={["Multiple locations"]}
-            position={["Engineering & IT"]}
-          />
-          <OpenPosition
-            company={{ name: "Optiver", logo: GoldmanSachsLogo }}
-            location={["Chicago, IL", "Austin, TX"]}
-            position={[
-              "Quantitative Trading",
-              "HFT Research",
-              "Software Engineering",
-            ]}
-          />
-          <OpenPosition
-            company={{ name: "Walmart", logo: GoldmanSachsLogo }}
-            location={["Multiple locations"]}
-            position={[
-              "Software Engineering",
-              "Systems Engineering",
-              "Cybersecurity",
-              "Data Science",
-            ]}
-          />
-          <OpenPosition
-            company={{ name: "Apple", logo: GoldmanSachsLogo }}
-            location={["Multiple locations"]}
-            position={["Software Engineering", "ML/AI", "Engineering PM"]}
-          />
-          <OpenPosition
-            company={{ name: "Neuralink", logo: GoldmanSachsLogo }}
-            location={["Fremont, CA", "Austin, TX"]}
-            position={["Software Engineering"]}
-          />
-          <OpenPosition
-            company={{ name: "Certik", logo: GoldmanSachsLogo }}
-            location={["Multiple locations"]}
-            position={["Development", "Platform Engineering", "Full-stack"]}
-          />
-          <OpenPosition
-            company={{ name: "D.E. Shaw", logo: GoldmanSachsLogo }}
-            location={["New York, NY"]}
-            position={[
-              "Software Development",
-              "Systems Technical PM",
-              "Systems Administration",
-              "Systems Technology",
-            ]}
-          />
-          <OpenPosition
-            company={{ name: "Bank of America", logo: GoldmanSachsLogo }}
-            location={["Multiple locations"]}
-            position={["Global Technology Analyst"]}
-          />
-          <OpenPosition
-            company={{ name: "Protiviti", logo: GoldmanSachsLogo }}
-            location={["Phoenix, AZ"]}
-            position={["Technology Consulting"]}
-          />
-          <OpenPosition
-            company={{ name: "Mercedes-Benz", logo: GoldmanSachsLogo }}
-            location={["Sunnyvale, CA"]}
-            position={["Data Products"]}
-          />
-          <OpenPosition
-            company={{ name: "LUMEN", logo: GoldmanSachsLogo }}
-            location={["Sunnyvale, CA"]}
-            position={["Various positions"]}
-          />
-          <OpenPosition
-            company={{ name: "JPMorgan Chase", logo: GoldmanSachsLogo }}
-            location={["Multiple locations"]}
-            position={["Software Engineering", "AI and Data Science"]}
-          />
-          <OpenPosition
-            company={{ name: "Citadel", logo: GoldmanSachsLogo }}
-            location={["Multiple locations"]}
-            position={[
-              "Software Engineering",
-              "Investment and Trading",
-              "Quant Research",
-            ]}
-          />
-        </div>
+        <OpenPosition />
       </div>
     </div>
   );
 }
-
